0145a2f41e0d9ad943ffaee1584c3d88acd10013
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-02-04 11:59:07 +0100

    Carga de comentarios cuando hacemos scroll

--- src/js/CommentsListManager.js
+++ src/js/CommentsListManager.js
@@ -21,26 +21,23 @@ module.exports = {
 
     loadComments: function() {
 
-        //Borramos todo antes de cargar
-        $(".list").empty();
-
         var self = this;
 
         // mostrar el mensaje de cargando
         self.setUiLoading();
 
+
         // cargamos los comentarios desde el backend
         CommentsService.list(function(comments){ // si nos devuelve comentarios
             if (comments.length == 0) {
                 self.setUiBlank(); // si no hay comentarios -> estado en blanco
-                $('.comments-number').html('').append(comments.length);
             } else {
                 // pintar los comentarios en el listado
                 self.renderComments(comments);
-                self.setUiIdeal(); // ponemos el estado ideal
+                self.setUiIdeal(); // ideal
             }
-        }, function(error){ // si se produce algún error
-            self.setUiError(); // ponemos el estado error
+        }, function(error){
+            self.setUiError(); // error
         });
     },
 
@@ -50,18 +47,21 @@ module.exports = {
           contentToAdd += '<div id="name-comment">' + comments[i].name + '&nbsp' + comments[i].lastName + '</div>' + '<li id="new-comment">' + comments[i].comment + '<button class="delete-button" data-id="' + comments[i].id + '" title="delete">&times;</button></li>';
         }
 
+        //Borramos todo antes de cargar
+        $(".list").empty();
         $(".list").append(contentToAdd);
         $('.comments-number').html('').append(comments.length);
+
+        return 8;
     },
 
     deleteComment: function(commentId) {
         var self = this;
         CommentsService.delete(commentId, function(){
-            // se ha borrado correctamente
+            alert('Se ha borrado correctamente');
             self.loadComments();
         }, function(){
-            // se ha producido un error al borrar
-            alert("No se ha podido eliminar el comentario");
+            alert("Error: No se ha podido eliminar el comentario");
         });
     }
 }
--- src/js/ready.js
+++ src/js/ready.js
@@ -1,24 +1,27 @@
 var $ = require('jquery');
 var CommentsListManager = require('./CommentsListManager');
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var offsetComments = $('#comments').offset();
+    var offsetComments = $('#comments').offset().top;
+    var load = false;
 
-  // cargar los comentarios
-  // $(window).scroll(function() {
-  //   if (window.pageYOffset >= offsetComments) {
-  //         CommentsListManager.loadComments();
-  //         console.log('ahora');
-  //     }
-  //   })
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() >= offsetComments && !load) {
+            load = true;
+            CommentsListManager.loadComments();
+        } else {
+            if (!load) {
+                CommentsListManager.setUiLoading();
+                console.log(CommentsListManager.renderComments());
+            }
+        }
+    })
 
-  CommentsListManager.loadComments();
-
-  // manejador de eventos del botón de borrar comentarios
-  $(".comments").on("click", ".delete-button", function(){
-      var commentId = $(this).data("id"); // recuperamos el id del comentario
-      CommentsListManager.deleteComment(commentId)
+    // manejador de eventos del botón de borrar comentarios
+    $(".comments").on("click", ".delete-button", function() {
+        var commentId = $(this).data("id"); // recuperamos el id del comentario
+        CommentsListManager.deleteComment(commentId)
     });
 
 });
